82c3a22a371d363c1b9f4e4cf1be9d5f530bffb7
Edwin HC <33665660+edwinrhc@users.noreply.github.com>
2023-10-06 17:33:51 -0500

    Segundo commit

---
+++ src/main/java/com/std/ec/controller/ClienteController.java
@@ -0,0 +1,9 @@
+package com.std.ec.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClienteController {
+
+
+}
--- src/test/java/com/std/ec/MiPrimerApiRestApplicationTests.java
+++ src/test/java/com/std/ec/MiPrimerApiRestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MiPrimerApiRestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
